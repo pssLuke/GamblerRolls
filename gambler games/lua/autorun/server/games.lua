17fe8edd752fe1a1eb26b225778a7b2dcc7f68d3
Luke <psslukespam@gmail.com>
2016-04-22 18:18:03 -0700

    Update games.lua

--- gambler games/lua/autorun/server/games.lua
+++ gambler games/lua/autorun/server/games.lua
@@ -1,7 +1,7 @@
 hook.Add( "PlayerSay", "gambler_roll", function( ply, text )
         if text:sub( 1, 5 ) == "/roll" and ply:Team() == TEAM_GAMBLER then
                 for k, v in pairs(player.GetAll()) do
-                	v:ChatPrint("Gambler rolled!")
+                	v:ChatPrint(ply:Nick().." rolled a "..math.random( 0, 100 ))
             	end
                 return ""
         end
